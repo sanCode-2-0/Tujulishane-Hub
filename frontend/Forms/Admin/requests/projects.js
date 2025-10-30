3404388c8899e43088bf2166bdda88abc56179f5
Braine Lomoni <82029545+code-briomar@users.noreply.github.com>
2025-10-30 04:35:27 +0300

    feat: Update API base URL to production for all frontend files

--- frontend/Forms/Admin/requests/projects.js
+++ frontend/Forms/Admin/requests/projects.js
@@ -4,8 +4,8 @@
 const API_BASE =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
-    ? "http://localhost:8080"
-    : "http://localhost:8080";
+    ? "https://tujulishane-hub-backend-52b7e709d99f.herokuapp.com/"
+    : "https://tujulishane-hub-backend-52b7e709d99f.herokuapp.com/";
 
 document.addEventListener("DOMContentLoaded", () => {
   const projectList = document.getElementById("projectList");
