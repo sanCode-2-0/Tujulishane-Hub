1928fb4dc4b758f9f3ebca824aa3117458f18dba
Braine Lomoni <82029545+code-briomar@users.noreply.github.com>
2025-11-03 13:42:00 +0300

    feat: Add county and sub-county coordinates for improved location mapping

--- frontend/kenya-locations.js
+++ frontend/kenya-locations.js
@@ -305,6 +305,115 @@ const kenyaLocations = {
   ],
 };
 
+// County coordinates (approximate center points)
+const countyCoordinates = {
+  Mombasa: [39.6645, -4.0435],
+  Kwale: [39.3091, -4.1734],
+  Kilifi: [39.9093, -3.6305],
+  "Tana River": [39.3735, -1.5628],
+  Lamu: [40.902, -2.2717],
+  "Taita-Taveta": [38.3454, -3.3161],
+  Garissa: [39.6367, -0.4563],
+  Wajir: [40.0573, 1.749],
+  Mandera: [40.1159, 3.9373],
+  Marsabit: [37.9899, 2.335],
+  Isiolo: [38.485, 0.3546],
+  Meru: [37.6559, 0.0515],
+  "Tharaka-Nithi": [37.94, -0.2962],
+  Embu: [37.4586, -0.5314],
+  Kitui: [38.0106, -1.366],
+  Machakos: [37.2624, -1.5167],
+  Makueni: [37.6203, -2.2515],
+  Nyandarua: [36.5229, -0.5531],
+  Nyeri: [36.9519, -0.4169],
+  Kirinyaga: [37.1208, -0.563],
+  "Murang'a": [37.1526, -0.7839],
+  Kiambu: [36.8304, -1.1714],
+  Turkana: [35.5286, 3.1199],
+  "West Pokot": [35.2718, 1.7809],
+  Samburu: [37.0862, 1.2179],
+  "Trans-Nzoia": [34.9449, 1.0561],
+  "Uasin Gishu": [35.3088, 0.5143],
+  "Elgeyo-Marakwet": [35.4786, 0.7806],
+  Nandi: [35.1118, 0.1836],
+  Baringo: [35.9662, 0.4662],
+  Laikipia: [36.7819, 0.3606],
+  Nakuru: [36.08, -0.3031],
+  Narok: [35.8601, -1.0783],
+  Kajiado: [36.782, -2.0981],
+  Kericho: [35.2863, -0.3677],
+  Bomet: [35.3416, -0.7833],
+  Kakamega: [34.7526, 0.2827],
+  Vihiga: [34.7275, 0.0765],
+  Bungoma: [34.5606, 0.5695],
+  Busia: [34.0971, 0.4608],
+  Siaya: [34.2697, 0.0677],
+  Kisumu: [34.7526, -0.0917],
+  "Homa Bay": [34.4608, -0.5273],
+  Migori: [34.4731, -1.0634],
+  Kisii: [34.7741, -0.6773],
+  Nyamira: [34.9354, -0.5669],
+  Nairobi: [36.8219, -1.2921],
+};
+
+// Sub-county coordinates (approximate center points)
+const subCountyCoordinates = {
+  // Mombasa
+  Changamwe: [39.628, -4.0167],
+  Jomvu: [39.6933, -4.0435],
+  Kisauni: [39.6167, -4.0167],
+  Likoni: [39.65, -4.0833],
+  Mvita: [39.6833, -4.0167],
+  Nyali: [39.6833, -4.0167],
+
+  // Kwale
+  Kinango: [39.3091, -4.1734],
+  "Lunga Lunga": [39.1167, -4.45],
+  Msambweni: [39.4667, -4.4667],
+  Matuga: [39.25, -4.1333],
+
+  // Kilifi
+  Ganze: [39.5, -3.5333],
+  Kaloleni: [39.6167, -3.8167],
+  "Kilifi North": [39.85, -3.6333],
+  "Kilifi South": [39.85, -3.6333],
+  Magarini: [39.9667, -3.0167],
+  Malindi: [40.1167, -3.2167],
+  Rabai: [39.6167, -3.9167],
+
+  // Nairobi (sample - can be expanded)
+  Westlands: [36.7833, -1.2667],
+  "Dagoretti North": [36.7667, -1.2833],
+  "Dagoretti South": [36.7667, -1.2833],
+  "Embakasi Central": [36.9, -1.3167],
+  "Embakasi East": [36.9, -1.3167],
+  "Embakasi North": [36.9, -1.3167],
+  "Embakasi South": [36.9, -1.3167],
+  "Embakasi West": [36.9, -1.3167],
+  Kamukunji: [36.8333, -1.2833],
+  Kasarani: [36.8833, -1.2333],
+  Kibra: [36.7833, -1.3167],
+  "Lang'ata": [36.7667, -1.3667],
+  Makadara: [36.8667, -1.3],
+  Mathare: [36.8667, -1.2833],
+  Roysambu: [36.8833, -1.2167],
+  Ruaraka: [36.8833, -1.25],
+  Starehe: [36.8167, -1.2833],
+
+  // Nakuru (sample)
+  Bahati: [36.0833, -0.2833],
+  Gilgil: [36.3167, -0.4833],
+  "Kuresoi North": [35.95, -0.2833],
+  "Kuresoi South": [35.95, -0.2833],
+  Molo: [35.7333, -0.25],
+  Naivasha: [36.4333, -0.7167],
+  "Nakuru Town East": [36.0667, -0.2833],
+  "Nakuru Town West": [36.0667, -0.2833],
+  Njoro: [35.9333, -0.3333],
+  Rongai: [35.8667, -0.4],
+  Subukia: [36.0833, -0.2],
+};
+
 // Function to populate sub-county dropdown based on selected county
 function populateSubCounties(countySelectId, subCountySelectId) {
   const countySelect = document.getElementById(countySelectId);
@@ -366,6 +475,8 @@ window.getSubCountiesForCounty = getSubCountiesForCounty;
 window.searchCounties = searchCounties;
 window.searchSubCounties = searchSubCounties;
 window.kenyaLocations = kenyaLocations;
+window.countyCoordinates = countyCoordinates;
+window.subCountyCoordinates = subCountyCoordinates;
 
 // Function to initialize county dropdown
 function initializeCountyDropdown(countySelectId, subCountySelectId) {
